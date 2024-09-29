a7311ba9bd4cf449b5e7f923c68e0d30e8dac6b5
torrin1 <torrin@worx4you.com>
2024-09-29 14:36:19 -0400

    FIxing webpack hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -6,70 +6,75 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { transformBabelAST } = await import("destam-dom/transform/htmlLiteral.js");
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const env = process.env.ENV;
+const env = process.env.NODE_ENV || 'development';
 
-const config = {
+const getWebpackConfig = async () => {
+  const { transformBabelAST } = await import("destam-dom/transform/htmlLiteral.js");
+
+  return {
     name: 'opengig.org',
     target: 'web',
     stats: 'minimal',
     devtool: 'source-map',
     mode: env,
     entry: {
-        opengig_org: './frontend/index.js',
+      opengig_org: [
+        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
+        './frontend/index.js'
+      ],
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/',
-        filename: 'index.js',
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: 'index.js',
     },
     resolve: {
-        extensions: ['.html', '.css', '.js', '.jsx'],
+      extensions: ['.html', '.css', '.js', '.jsx'],
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './frontend/index.html',
-        }),
+      new HtmlWebpackPlugin({
+        template: './frontend/index.html',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
     ],
     module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: [
-                            '@babel/plugin-syntax-jsx',
-                            () => ({
-                                visitor: {
-                                    Program: path => {
-                                        transformBabelAST(path.node);
-                                    }
-                                }
-                            })
-                        ],
-                    }
-                }
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-syntax-jsx',
+                () => ({
+                  visitor: {
+                    Program(path) {
+                      transformBabelAST(path.node);
+                    },
+                  },
+                }),
+              ],
             },
-            {
-                test: /\.(png|jpg|jpeg|gif|ico|webp)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext][query]',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ]
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|ico|webp)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name][ext][query]',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
-};
-  
-  
-export default config;
+  };
+}
+
+export default await getWebpackConfig();
