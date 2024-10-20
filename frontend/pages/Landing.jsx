af227a4fa682d29ad1c1a9bce6af59ec31c2b449
torrin1 <torrin@worx4you.com>
2024-10-20 11:05:49 -0400

    simplifying landing

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -1,34 +1,19 @@
-import { h, Markdown, Button, Typography } from 'destamatic-ui';
-import { Observer } from 'destam-dom';
-// import readmeContent from '../../README.md';
+import { h, Button, Typography } from 'destamatic-ui';
 
 const Header = ({}) => {
-    const hover = Observer.mutable(false);
     return <div style={{
             position: 'sticky',
             top: 0,
             backgroundColor: 'white',
-            zIndex: 1000,
             padding: '10px',
             display: 'flex',
             justifyContent: 'space-between',
             borderBottom: '1px solid #ddd'
         }}>
         <div style={{ position: 'relative', display: 'inline-block' }}>
-          <Typography type="h4" onMouseEnter={() => hover.set(true)} onMouseLeave={() => hover.set(false)}>
+          <Typography type="h4">
               OpenGig
           </Typography>
-          <div
-            style={{
-              backgroundColor: "black",
-              width: hover.map(h => h ? '100%' : '0%'),
-              height: '4px',
-              position: 'absolute',
-              bottom: '-2px',
-              left: 0,
-              transition: 'width 0.6s cubic-bezier(0.2,0.5,0.6,1)',
-            }}
-          />
         </div>
         <Button
             label='Sign up'
@@ -46,7 +31,6 @@ const Header = ({}) => {
 const Landing = ({ OClient }) => {
     return <div>
         <Header />
-        {/* <Markdown markdown={readmeContent} /> */}
     </div>;
 };
 
