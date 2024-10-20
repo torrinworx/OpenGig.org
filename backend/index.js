3932730580d9b806d8424a1cf80db3f960d7494a
torrin1 <torrin@worx4you.com>
2024-10-20 11:14:39 -0400

    cleaning up backend

--- backend/index.js
+++ backend/index.js
@@ -13,40 +13,36 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-async function startServer() {
-  if (process.env.ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')));
-    app.get('*', (_req, res) => {
-      res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-    });
-  } else {
-    const vite = await createViteServer({
-      server: { middlewareMode: 'html' }
-    });
+if (process.env.ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../build')));
+	app.get('*', (_req, res) => {
+		res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+	});
+} else {
+	const vite = await createViteServer({
+		server: { middlewareMode: 'html' }
+	});
 
-    app.use(vite.middlewares);
+	app.use(vite.middlewares);
 
-    app.get('*', async (req, res, next) => {
-      try {
-        const url = req.originalUrl;
-        const template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
-        const html = await vite.transformIndexHtml(url, template);
+	app.get('*', async (req, res, next) => {
+		try {
+			const url = req.originalUrl;
+			const template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+			const html = await vite.transformIndexHtml(url, template);
 
-        res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-      } catch (e) {
-        vite.ssrFixStacktrace(e);
-        next(e);
-      }
-    });
-  }
-
-  const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => {
-      console.log(`Serving on http://localhost:${port}/`);
-    });
-
-  // Attach any required socket or websocket connections
-  connection(server);
+			res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+		} catch (e) {
+			vite.ssrFixStacktrace(e);
+			next(e);
+		}
+	});
 }
 
-startServer();
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+	console.log(`Serving on http://localhost:${port}/`);
+});
+
+// Attach any required socket or websocket connections
+connection(server);
