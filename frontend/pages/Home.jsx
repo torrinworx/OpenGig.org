05c001a9ca8a3f6e0a43812ae69067fd9cc8441b
torrin1 <torrin@worx4you.com>
2024-10-20 11:14:32 -0400

    adding auth stub

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,18 +1,28 @@
-import { Theme, Button } from 'destamatic-ui';
+import { h, Theme, Button, Shown } from 'destamatic-ui';
+import Auth from '../components/Auth';
+
 
 const Home = Theme.use(theme => ({OClient}) => {
     const counter = OClient.observer.path('counter').def(0);
+    OClient.authenticated = false;
 
     return <div>
-        Hello World
-        <br />
-        {counter}
-        <Button
-            label='Click'
-            onMouseDown={() => {
-                counter.set(counter.get() + 1);
-            }}
-        />
+        <Shown value={OClient.authenticated} invert>
+            <Auth Oclient={OClient} />
+        </Shown>
+
+        <Shown value={OClient.authenticated}>
+            Hello World
+            <br />
+            {counter}
+            <Button
+                label='Click'
+                onMouseDown={() => {
+                    counter.set(counter.get() + 1);
+                }}
+            />
+        </Shown>
+
     </div>;
 });
 
