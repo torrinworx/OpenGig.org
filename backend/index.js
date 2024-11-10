25f01d0d5de779ae956f98f942990a58766dae0f
torrin1 <torrin@worx4you.com>
2024-11-10 18:14:30 -0500

    Trying to fix build

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import { OObject } from "destam"
 import { ODB, coreServer } from "web-core/server";
-
+import path from 'path';
 
 const connection = async (ws, req) => {
     console.log("This is running on connection")
     return
 };
-
-coreServer('./backend/jobs', './frontend/build', connection);
+console.log(path.resolve('./frontend/build'));
+coreServer(
+    path.resolve('./backend/jobs'),
+    path.resolve('./frontend/build'),
+    connection
+);
