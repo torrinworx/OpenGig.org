adcb2c31d7e5e4354c73882e31158dadffb70e8c
torrin1 <torrin@worx4you.com>
2024-11-11 21:30:40 -0500

    Fixing build

--- build.sh
+++ build.sh
@@ -6,15 +6,26 @@ if [ -d "./build" ]; then
 	rm -rf ./build
 fi
 
+# Assumes git submodules are updated to the correct commit.
 npm i
 npx vite build
 
-npm i --production
-
 rm -rf ./web-core/.git
+rm -rf ./web-core/node_modules
 rm -rf ./destamatic-ui/.git
+rm -rf ./destamatic-ui/node_modules
 
 mkdir -p ./build
+
 cp -r ./backend ./build
 cp -r ./web-core ./build
 cp -r ./destamatic-ui ./build
+cp ./package.json ./build
+cp ./package-lock.json ./build
+
+cat << 'EOF' > ./build/run.sh
+#!/bin/bash
+node ./backend/index.js
+EOF
+
+chmod +x ./build/run.sh
