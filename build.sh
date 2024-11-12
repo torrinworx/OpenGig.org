a1b995d29dee0070dcca4b79da8e93e5be761d54
torrin1 <torrin@worx4you.com>
2024-11-11 23:27:12 -0500

    trying to fix stuff

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-if [ -d "./build" ]; then
-	rm -rf ./build
+BUILD_DIR="./build"
+ZIP_FILE="./build.zip"
+
+if [ -d "$BUILD_DIR" ]; then
+	rm -rf "$BUILD_DIR"
+fi
+
+if [ -f "$ZIP_FILE" ]; then
+	rm -f "$ZIP_FILE"
 fi
 
 # Assumes git submodules are updated to the correct commit.
@@ -15,17 +22,20 @@ rm -rf ./web-core/node_modules
 rm -rf ./destamatic-ui/.git
 rm -rf ./destamatic-ui/node_modules
 
-mkdir -p ./build
+mkdir -p "$BUILD_DIR"
 
-cp -r ./backend ./build
-cp -r ./web-core ./build
-cp -r ./destamatic-ui ./build
-cp ./package.json ./build
-cp ./package-lock.json ./build
+cp -r ./backend "$BUILD_DIR"
+cp -r ./web-core "$BUILD_DIR"
+cp -r ./destamatic-ui "$BUILD_DIR"
+cp ./package.json "$BUILD_DIR"
+cp ./package-lock.json "$BUILD_DIR"
 
-cat << 'EOF' > ./build/run.sh
+cat << 'EOF' > "$BUILD_DIR/run.sh"
 #!/bin/bash
 node ./backend/index.js
 EOF
 
-chmod +x ./build/run.sh
+chmod +x "$BUILD_DIR/run.sh"
+
+# Zip the build directory
+zip -r "$ZIP_FILE" "$BUILD_DIR"
