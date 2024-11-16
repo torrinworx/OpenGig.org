9da1899c09959a469b9efbd352efdc770ed85987
torrin1 <torrin@worx4you.com>
2024-11-16 11:04:03 -0500

    removing redundent not found

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,11 @@
-import { OObject } from "destam";
 import { coreServer } from "web-core/server";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const connection = async (ws, req) => {
 	return;
 };
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const rootDir = path.resolve(__dirname, '..');
-console.log('Root Directory:', rootDir);
-
 coreServer(
-	path.resolve('./backend/jobs'),
+	'./backend/jobs',
 	process.env.ENV === 'production' ? './dist' : './frontend',
 	connection
 );
--- frontend/pages/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-    return <div>
-        404 not found OpenGig.org
-    </div>;
-};
-
-export default NotFound;
