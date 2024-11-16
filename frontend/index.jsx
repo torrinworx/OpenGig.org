99ed0088fa2307a592a1737851cac9dc6b3b3741
torrin1 <torrin@worx4you.com>
2024-11-16 12:39:51 -0500

    test code for pages

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,34 +1,67 @@
-import { OObject } from 'destam-dom';
+import { OObject, Observer } from 'destam-dom';
 import { coreClient, jobRequest, getCookie } from 'web-core/client';
-import { Shown, Theme, Typography } from 'destamatic-ui';
+import { Theme, Typography } from 'destamatic-ui';
 
 import theme from './theme';
 import Auth from './components/Auth';
 import Landing from './pages/Landing';
 // import Notifications from './components/Notifications';
 
+const Pages = ({ state }) => {
+    const pages = import.meta.glob('./pages/*.jsx');
+
+    return state.client.observer.path('openPage').map(p => {
+        const matchedPath = Object.keys(pages).find(filePath => {
+            const parts = filePath.split('/');
+            const fileName = parts[parts.length - 1].replace('.jsx', '');
+            return fileName === p.page;
+        });
+        console.log(matchedPath)
+        console.log(pages)
+        if (matchedPath) {
+            const Page = pages[matchedPath];
+            console.log(Page);
+            return <Page {...{state, ...p.props}}/> 
+        } else return null // Might want to throw error that this component wasn't found
+    });
+};
+
 const App = ({ state }) => {
+
     state.client = OObject({
         authenticated: false,
-        openPage: { page: Landing }
+        openPage: { page: "Landing" }
     })
 
     const token = getCookie('webCore') || '';
-	if (token) {
+    if (token) {
         (async () => await jobRequest('sync'))();
-        state.client.observer.path('openPage').set({page: Auth})
+        state.client.observer.path('openPage').set({ page: "Auth" })
         state.client.observer.path('authenticated').set(true)
 
     };
 
+    // Observer.timer(500).watch(() => {
+    //     const openPage = state.client.observer.path('openPage')
+    //     console.log(openPage.get().page)
+    //     if (openPage.get().page === 'Landing') {
+    //         openPage.set({ page: "test" })
+    //     } else {
+    //         openPage.set({ page: "Landing" })
+    //     }
+    // });
+
+
+    state.client.observer.path('openPage').watch(d => console.log(d.value));
+
     return <Theme value={theme}>
         {/* <Notifications state={state} /> */}
-        <Shown value={state.client.observer.path('openPage')}>
-            {state.client.observer.path('openPage').map(p => {
+        {/* {state.client.observer.path('openPage').map(p => {
                 const Page = p.page;
                 if (Page) return <Page {...{ state, ...p.props }} />;
-            })}
-        </Shown>
+            })} */}
+
+        <Pages state={state} />
     </Theme>;
 };
 
