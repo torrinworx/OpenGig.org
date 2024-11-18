0855b24f3a6993c22e877008fa59e20085bc88e1
torrin1 <torrin@worx4you.com>
2024-11-17 22:59:17 -0500

    Fixing Pages imports

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,33 +1,49 @@
-import { OObject, Observer } from 'destam-dom';
+import { OObject } from 'destam-dom';
 import { coreClient, jobRequest, getCookie } from 'web-core/client';
-import { Theme, Typography } from 'destamatic-ui';
+import { Button, Theme, Typography } from 'destamatic-ui';
 
 import theme from './theme';
-import Auth from './components/Auth';
-import Landing from './pages/Landing';
-// import Notifications from './components/Notifications';
+import Notifications from './components/Notifications';
 
+const pages = import.meta.glob('./pages/*.jsx', { eager: true }); // Use 'eager: true' to load all files at once
+
+const NotFound = () => <Theme value={theme}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Typography type='h4' style={{ marginBottom: '20px' }}>404 Page Not Found</Typography>
+        <Typography type='p1' style={{ marginBottom: '20px' }}>The page you are trying to access is either unavailable or restricted.</Typography>
+        <Button
+            type='contained'
+            label='Return to Site'
+            onMouseDown={() => {
+                state.client.observer.path('openPage').set({ page: "Landing" });
+                window.location.href = '/';
+            }}
+        />
+    </div>
+</Theme>;
+
+// Pages, openPage helps avoid circular import issues when trying to just simply set the page on a button click.
 const Pages = ({ state }) => {
-    const pages = import.meta.glob('./pages/*.jsx');
+    const openPage = state.client.observer.path('openPage');
 
-    return state.client.observer.path('openPage').map(p => {
+    return openPage.map((p) => {
         const matchedPath = Object.keys(pages).find(filePath => {
             const parts = filePath.split('/');
             const fileName = parts[parts.length - 1].replace('.jsx', '');
             return fileName === p.page;
         });
-        console.log(matchedPath)
-        console.log(pages)
+
         if (matchedPath) {
-            const Page = pages[matchedPath];
-            console.log(Page);
-            return <Page {...{state, ...p.props}}/> 
-        } else return null // Might want to throw error that this component wasn't found
-    });
+            const PageFunc = pages[matchedPath];
+            const Page = PageFunc.default;
+            return <Page {...{ state, ...p.props }} />;
+        }
+
+        return <NotFound />;
+    })
 };
 
 const App = ({ state }) => {
-
     state.client = OObject({
         authenticated: false,
         openPage: { page: "Landing" }
@@ -41,32 +57,10 @@ const App = ({ state }) => {
 
     };
 
-    // Observer.timer(500).watch(() => {
-    //     const openPage = state.client.observer.path('openPage')
-    //     console.log(openPage.get().page)
-    //     if (openPage.get().page === 'Landing') {
-    //         openPage.set({ page: "test" })
-    //     } else {
-    //         openPage.set({ page: "Landing" })
-    //     }
-    // });
-
-
-    state.client.observer.path('openPage').watch(d => console.log(d.value));
-
     return <Theme value={theme}>
-        {/* <Notifications state={state} /> */}
-        {/* {state.client.observer.path('openPage').map(p => {
-                const Page = p.page;
-                if (Page) return <Page {...{ state, ...p.props }} />;
-            })} */}
-
+        <Notifications state={state} />
         <Pages state={state} />
     </Theme>;
 };
-
-const NotFound = () => <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <Typography type='h4'>Not Found 404</Typography>
-</div>;
 
 coreClient(App, NotFound);
