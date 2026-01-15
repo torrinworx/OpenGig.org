39f4c750f9d9fa0c4fe224b3d09abe2d3d787ee7
Torrin Leonard <torrin@torrin.me>
2026-01-14 21:55:51 -0500

    Updates to home

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { Observer, OArray } from "destam-dom";
 import { modReq } from 'destam-web-core/client';
-import { Theme, Button, Paper, Typography, TextField, Icon, Detached, StageContext } from "destamatic-ui";
+import { Theme, Button, Paper, Typography, TextField, Icon, Detached, StageContext, suspend, LoadingDots } from "destamatic-ui";
+
+import AppContext from '../utils/appContext.js';
 
 import LogoLightMode from '/branding/OpenGig_Logo_Light_Mode.svg';
 import LogoDarkMode from '/branding/OpenGig_Logo_Dark_Mode.svg';
@@ -19,57 +21,6 @@ Theme.define({
 
 	gigtile_hovered: {
 		background: '$color_hover'
-	}
-});
-
-const Gig = StageContext.use(m => {
-	return ({ each: gig }) => {
-		const hover = Observer.mutable(false);
-
-		return <Button style={{ padding: 0 }} onClick={() => m.open({ name: 'Gig', label: gig.name, gig })}>
-			<div
-				theme={[
-					'radius',
-					'gigtile',
-					hover.map(h => h ? 'hovered' : null),
-				]}
-				isHovered={hover}
-			>
-				<div theme='center_radius' style={{
-					height: '150px',
-					backgroundColor: '$color_top',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					flexShrink: 0
-				}}>
-					<Icon name='image' size={20} style={{ color: '$color_main' }} />
-				</div>
-				<div style={{
-					display: 'flex',
-					alignItems: 'center',
-					gap: '10px',
-					flexShrink: 0
-				}}>
-					<div style={{
-						width: '25px',
-						height: '25px',
-						backgroundColor: '$color_top',
-						borderRadius: '50%',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}>
-						<Icon name='user' size={20} style={{ color: '$color_main' }} />
-					</div>
-					<Typography type='p1' label={gig.userName} style={{ color: '$color_top' }} />
-				</div>
-				<div theme='column' style={{ flexGrow: 1 }}>
-					<Typography type='h6_bold' label={gig.name} style={{ color: '$color_top', textAlign: 'left' }} />
-					<Typography type='p2' label={gig.tagline} style={{ color: '$color_top', textAlign: 'left' }} />
-				</div>
-			</div>
-		</Button>;
 	}
 });
 
@@ -122,58 +73,128 @@ const SearchBar = () => {
 	</div>;
 };
 
-const Kebab = ({ children, ...props }) => {
+const Kebab = ({ children }) => {
 	const focused = Observer.mutable(false);
 
-	return <Detached enabled={focused}>
+	return <Detached>
 		<Button
-			type='icon'
+			type='text'
 			onClick={() => focused.set(!focused.get())}
-			style={{
-				padding: 0,
-				height: 40,
-				width: 40,
-				borderRadius: 50,
-				flexShrink: 0,
-			}}
 			title='Menu'
-			icon={<Icon name='menu' size={30} />}
+			icon={<Icon name='feather:menu' size={30} />}
 		/>
 		<mark:popup>
-			<Paper {...props}>
+			<Paper theme='column' style={{ padding: 8, gap: 8 }}>
 				{children}
 			</Paper>
 		</mark:popup>
-	</Detached>
+	</Detached>;
 };
 
-const Home = StageContext.use(s => ({ state }) => {
-	const gigs = OArray([]);
+const Gig = StageContext.use(s => ({ each: gigId, gigs }) => {
+	console.log(gigId);
+	console.log(gigs);
 
-	// todo have this somehow be a mapped network? or maybe just run this all through state. not sure why we would use mapped network here.
-	const getGigs = async () => {
-		const response = await modReq('gigs/Get');
-		gigs.push(...response.gigs);
-	};
+	const gig = gigs[gigId];
 
+	const hover = Observer.mutable(false);
 
-	// TODO: Move gigs thing to a suspended component.
-	getGigs();
+	return <Button style={{ padding: 0 }} onClick={() => s.open({ name: 'Gig', gigId })}>
+		<div
+			theme={[
+				'radius',
+				'gigtile',
+				hover.map(h => h ? 'hovered' : null),
+			]}
+			isHovered={hover}
+		>
+			<div theme='center_radius' style={{
+				height: '150px',
+				backgroundColor: '$color',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				flexShrink: 0
+			}}>
+				<Icon name='feather:image' size={20} style={{ color: '$color_background' }} />
+			</div>
+			<div style={{
+				display: 'flex',
+				alignItems: 'center',
+				gap: '10px',
+				flexShrink: 0
+			}}>
+				<div style={{
+					width: '25px',
+					height: '25px',
+					backgroundColor: '$color',
+					borderRadius: '50%',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}>
+					<Icon name='user' size={20} style={{ color: '$color_background' }} />
+				</div>
+				<Typography type='p1' label={gig.userName} style={{ color: '$color_background' }} />
+			</div>
+			<div theme='column' style={{ flexGrow: 1 }}>
+				<Typography type='h6_bold' label={gig.name} style={{ color: '$color_background', textAlign: 'left' }} />
+				<Typography type='p2' label={gig.tagline} style={{ color: '$color_background', textAlign: 'left' }} />
+			</div>
+		</div>
+	</Button>;
+});
+
+const Gigs = suspend(LoadingDots, async () => {
+	const gigs = await modReq('gigs/Get')
+
+	const gigKeys = OArray(Object.keys(gigs));
+
+	return <div theme='row_wrap_contentContainer' style={{ gap: 10 }}>
+		<Gig each={gigKeys} gigs={gigs} />
+	</div>;
+});
+
+const Home = AppContext.use(app => StageContext.use(s => () => {
 
 	return <>
-		<div theme='row_fill_wrap_spread_contentContainer'>
-			<img
-				src={window.themeMode.map(t => t === false ? LogoLightMode : LogoDarkMode)}
-				style={{
-					width: '20vw',
-					maxWidth: 260,
-					minWidth: 200,
-					height: 'auto',
-					objectFit: 'cover',
-					display: 'block',
-				}}
-			/>
-			<div theme='row_wrap' style={{ gap: 10 }}>
+		<div theme='column_fill_contentContainer' style={{ gap: 40 }}>
+			<div theme='row_fill_spread_wrap' style={{ gap: 10 }}>
+				<img
+					src={window.themeMode.map(t => t === false ? LogoLightMode : LogoDarkMode)}
+					style={{
+						width: '20vw',
+						maxWidth: 260,
+						minWidth: 200,
+						height: 'auto',
+						objectFit: 'cover',
+						display: 'block',
+					}}
+				/>
+				<Kebab style={{}} theme='column_tight_center'>
+					<Button
+						title='Account'
+						type='text'
+						onClick={() => s.open({ name: 'Account' })}
+						icon={<Icon name='feather:user' size={30} />}
+					/>
+					{/* <Button
+						type='contained'
+						onMouseDown={async () => state.modal.set({ name: 'StripeTest', header: 'Stripe Test' })}
+						label='Stripe setup'
+					/> */}
+					<Button
+						title='Log Out'
+						type='text'
+						onClick={() => {
+							app.get().leave();
+							s.open({ name: 'Landing' });
+						}}
+						icon={<Icon name='feather:log-out' size={30} />}
+					/>
+				</Kebab>
+			</div>
+			<div theme='row_fill_center_wrap' style={{ gap: 10 }}>
 				<Button
 					title='Create a Gig'
 					label='Create'
@@ -187,10 +208,8 @@ const Home = StageContext.use(s => ({ state }) => {
 				<SearchBar />
 			</div>
 		</div>
-		<div theme='row_wrap_contentContainer' style={{ gap: 10 }}>
-			<Gig each={gigs} state={state} />
-		</div>
+		<Gigs />
 	</>;
-});
+}));
 
 export default Home;
