98ebeb76d6e9e0da0548b162c7d63fedb7f32d8d
Torrin Leonard <torrin@torrin.me>
2026-01-18 17:37:52 -0500

    Fixing back button

--- frontend/pages/NotFound.jsx
+++ frontend/pages/NotFound.jsx
@@ -1,9 +1,15 @@
-import { Button, Typography, StageContext } from 'destamatic-ui';
+import { Button, Typography, Icon } from 'destamatic-ui';
 
-const NotFound = StageContext.use(s => () => <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+const NotFound = () => <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
 	<Typography type='h1' style={{ marginBottom: '20px' }}>404 Page Not Found</Typography>
 	<Typography type='p1' style={{ marginBottom: '20px' }}>The page you are trying to access is either unavailable or restricted.</Typography>
-	<Button type='outlined' label='Go back' onClick={() => s.open({ name: 'landing' })} />
-</div>);
+
+	<Button
+		label="Back"
+		icon={<Icon name='feather:arrow-left' />}
+		type="contained"
+		onClick={() => history.back()}
+		/>
+</div>;
 
 export default NotFound;
