f84232fd9be113005f48c95a6abb4d2365daddf0
Torrin Leonard <torrin@torrin.me>
2026-01-18 09:55:11 -0500

    Trying to fix nginx websocket timeout issue

--- setup.sh
+++ setup.sh
@@ -175,6 +175,12 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+
+        proxy_read_timeout 3600s;
+        proxy_send_timeout 3600s;
+        proxy_connect_timeout 60s;
+        send_timeout 3600s;
+
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
