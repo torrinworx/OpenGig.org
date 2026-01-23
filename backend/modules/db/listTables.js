0fc876e2cd1a344f140b835a9770478306b0b4fa
Torrin Leonard <torrin@torrin.me>
2026-01-22 21:09:58 -0500

    Adding list tables backend module

---
+++ backend/modules/db/listTables.js
@@ -0,0 +1,24 @@
+export default () => {
+	return {
+		onMsg: async (_, __, { user, database }) => {
+			if (user?.query?.role !== 'admin') {
+				return { error: 'forbidden' };
+			}
+
+			if (!database) {
+				throw new Error('Mongo database not provided (context.database)');
+			}
+
+			const cols = await database
+				.listCollections({}, { nameOnly: true })
+				.toArray();
+
+			const tables = cols
+				.map(c => c.name)
+				.filter(name => name && name !== 'DBTableDataStore')
+				.sort();
+
+			return tables;
+		}
+	};
+};
