4502088edbb323ee6b3e408a7389adea044b77b0
Torrin Leonard <torrin@torrin.me>
2026-02-06 17:54:20 -0500

    removing console.log

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -86,7 +86,6 @@ const Auth = StageContext.use(s => suspend(Stasis, async () => {
 		// wait for reconnect + auth packet
 		await state.authKnown.defined(v => v === true);
 
-		console.log(response)
 		if (state.authed.get()) s.open({ name: 'home' });
 		else error.set('Login failed. Please try again.');
 	};
