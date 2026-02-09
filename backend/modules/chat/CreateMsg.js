23c904f4b0ce9e04e055643e1a4357afda7b68e8
Torrin Leonard <torrin@torrin.me>
2026-02-09 16:29:14 -0500

    Updating createmsg to store created/modiifed dates

--- backend/modules/chat/CreateMsg.js
+++ backend/modules/chat/CreateMsg.js
@@ -1,30 +1,30 @@
 import { OObject } from 'destam';
 
-const pushChange = (chat, change) => {
-	chat.seq = (chat.seq ?? 0) + 1;
-	const evt = { ...change, seq: chat.seq, at: Date.now() };
-	chat.change = evt;
-};
-
 export default () => ({
 	onMsg: async ({ chatId, text = '' }, { odb, user }) => {
+		const now = new Date();
+
 		const chat = await odb.findOne({
 			collection: 'chats',
 			query: { id: chatId },
 		});
-		
+
 		const msg = await odb.open({
 			collection: 'messages',
 			value: OObject({
 				chatId,
 				creator: user.observer.id.toHex(),
 				text: String(text),
+				createdAt: now,
+				modifiedAt: now,
 			}),
 		});
 
 		await msg.$odb.flush();
 
-		pushChange(chat, { type: 'create', msgId: msg.observer.id.toHex() });
+		chat.seq = (chat.seq ?? 0) + 1;
+		chat.modifiedAt = now;
+
 		await chat.$odb.flush();
 	},
 });
