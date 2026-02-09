d66cff192c3b76ff0ad6695e9efd956309b0c200
Torrin Leonard <torrin@torrin.me>
2026-02-09 15:56:55 -0500

    Simplifying CreateChat.js

--- backend/modules/chat/CreateChat.js
+++ backend/modules/chat/CreateChat.js
@@ -2,25 +2,19 @@ import { OArray, OObject } from 'destam';
 
 export default () => ({
 	onMsg: async ({ participants, title = 'New Chat' }, { odb, user }) => {
-		if (!odb) throw new Error('createChat.onMsg requires ctx.odb');
 
 		const list = Array.isArray(participants) ? [...participants] : [participants];
-		const myId = user.observer.id.toHex();
-		if (!myId) throw new Error('createChat.onMsg requires user.id');
+		const creator = user.observer.id.toHex();
 
-		if (!list.includes(myId)) list.push(myId);
+		if (!list.includes(creator)) list.push(creator);
 
 		const chat = await odb.open({
 			collection: 'chats',
 			value: OObject({
 				title,
 				participants: OArray(list),
-				creatorId: myId,
-
-				seq: 0,
-				changes: OArray(),
-				createdAt: Date.now(),
-				modifiedAt: Date.now(),
+				creator: creator,
+				seq: 0
 			}),
 		});
 
